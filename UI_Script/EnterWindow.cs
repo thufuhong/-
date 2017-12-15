9de69a3d467b7f7ad28ee160ccbfc3f1cb344164
Zhiyuan-Wu <86562713@qq.com>
2017-12-15 16:30:51 +0800

    Better Introduction Window

--- UI_Script/EnterWindow.cs
+++ UI_Script/EnterWindow.cs
@@ -26,32 +26,65 @@ public class EnterWindow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// other curIdx
-		if (curIdx > 2) {
+		if (curIdx > 5) {
 			return;
 		}
 		// stop
 		Time.timeScale = 0;
 		//
-		if (curIdx == 2) {
+		if (curIdx == 5) {
+            
 			Button_next_text.text = "下一步";
 			Button_quit_text.text = "退出游戏";
 			message.text = message_1;
-		}
-		if (curIdx == 1) {
-			Button_next_text.text = "下一步";
-			Button_quit_text.text = "退出游戏";
-			message.text = message_2;
-		}
-		if (curIdx == 0) {
-			Button_next_text.text = "开始游戏";
-			Button_quit_text.text = "退出游戏";
-			message.text = message_3;
-		}
-		if (curIdx < 0) {
+            transform.Find("EnterWindow").Find("Slide3").gameObject.SetActive(true);
+        }
+        if (curIdx == 4)
+        {
+            Button_next_text.text = "下一步";
+            Button_quit_text.text = "退出游戏";
+            message.text = message_1;
+            transform.Find("EnterWindow").Find("Slide3").gameObject.SetActive(false);
+            transform.Find("EnterWindow").Find("Slide1").gameObject.SetActive(true);
+        }
+        if (curIdx == 3)
+        {
+            Button_next_text.text = "下一步";
+            Button_quit_text.text = "退出游戏";
+            message.text = message_1;
+            transform.Find("EnterWindow").Find("Slide1").gameObject.SetActive(false);
+            transform.Find("EnterWindow").Find("Slide2").gameObject.SetActive(true);
+        }
+        if (curIdx == 2)
+        {
+            Button_next_text.text = "下一步";
+            Button_quit_text.text = "退出游戏";
+            message.text = message_1;
+            transform.Find("EnterWindow").Find("Slide2").gameObject.SetActive(false);
+            transform.Find("EnterWindow").Find("Slide5").gameObject.SetActive(true);
+        }
+        if (curIdx == 1)
+        {
+            Button_next_text.text = "下一步";
+            Button_quit_text.text = "退出游戏";
+            message.text = message_1;
+            transform.Find("EnterWindow").Find("Slide5").gameObject.SetActive(false);
+            transform.Find("EnterWindow").Find("Slide4").gameObject.SetActive(true);
+        }
+        if (curIdx == 0)
+        {
+            Button_next_text.text = "开始游戏";
+            Button_quit_text.text = "退出游戏";
+            message.text = message_1;
+            transform.Find("EnterWindow").Find("Slide4").gameObject.SetActive(false);
+            transform.Find("EnterWindow").Find("Slide6").gameObject.SetActive(true);
+        }
+        if (curIdx < 0) {
 			// continue
 			Time.timeScale = 1;
-			// wait for next using init function
-			curIdx = 100;
+            transform.Find("Mask1").gameObject.SetActive(false);
+            // wait for next using init function
+            curIdx = 100;
 			// 
 			enterWindow.SetActive (false);
 		}
@@ -65,39 +98,46 @@ public class EnterWindow : MonoBehaviour {
 	// click Button_quit
 	public void Button_quit() {
 		Time.timeScale = 1;
+
 		UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
 	}
 
 	// init : if enter a new level
-	public void InitFunction() {
-		// dynamic binding
-		enterWindow = transform.Find ("EnterWindow").gameObject;
-		message = enterWindow.transform.Find ("Message").GetComponent<Text> ();
-		axe_econ = transform.Find ("/axe_econ").gameObject;
-		Button_next_text = enterWindow.transform.Find ("Button_next").transform.Find ("Text").GetComponent<Text> (); 
-		Button_quit_text = enterWindow.transform.Find ("Button_quit").transform.Find ("Text").GetComponent<Text> ();
-		enterWindow.SetActive (true);
-		curIdx = 2;
-		message.supportRichText = true;
-		hero_value = axe_econ.GetComponent<attribute> ();
+	public void InitFunction() 
+	{
+		int level_num = PlayerPrefs.GetInt ("level_num", 1);
+		if (level_num == 1)
+		{
+			// dynamic binding
+			enterWindow = transform.Find ("EnterWindow").gameObject;
+			message = enterWindow.transform.Find ("Message").GetComponent<Text> ();
+			axe_econ = transform.Find ("/axe_econ").gameObject;
+			Button_next_text = enterWindow.transform.Find ("Button_next").transform.Find ("Text").GetComponent<Text> (); 
+			Button_quit_text = enterWindow.transform.Find ("Button_quit").transform.Find ("Text").GetComponent<Text> ();
+			enterWindow.SetActive (true);
+			curIdx = 5;
+			message.supportRichText = true;
+			hero_value = axe_econ.GetComponent<attribute> ();
 
-		// stop
-		Time.timeScale = 0;
-		// init main message
-		message_1 = "\n<b><size=15>    2017年秋季学期 <color=yellow>软件工程</color> <color=red>喜迎十九大</color> 小组大作业：</size></b>"
-			+ "\n\n\n<b><size=25>        没有名字的多风格混合游戏 </size></b>"
+			// stop
+			Time.timeScale = 0;
+            transform.Find("Mask1").gameObject.SetActive(true);
+            // init main message
+            message_1 = "\n<b><size=15>    2017年秋季学期 <color=yellow>软件工程</color> <color=red>喜迎十九大</color> 小组大作业：</size></b>"
+			+ "\n\n\n<b><size=25>        多风格混合游戏 </size></b>"
 			+ "\n\n<b><size=15>    团队成员：付宏、叶豪、牟宏杰、武智源、慕思成</size></b>"
 			+ "\n<b><size=15>    源码地址：<color=blue>https://github.com/thufuhong/-</color></size></b>";
-		message_2 = "\n<b><size=15>     <color=red>关卡：" + SceneManager.GetActiveScene().name + "</color> </size></b>"
+			message_2 = "\n<b><size=15>     <color=red>关卡：" + SceneManager.GetActiveScene ().name + "</color> </size></b>"
 			+ "\n\n<b><size=15>        " + hero_value.NiCheng + "，欢迎您进入了青青草原</size></b>"
 			+ "\n<b><size=15>        击败 <color=blue>boss（蓝色水晶）</color>是过关的充要条件</size></b>"
 			+ "\n<b><size=15>        击杀 <color=green>怪物（比你更好看的小人）</color>获得经验和金钱</size></b>"
 			+ "\n<b><size=15>        <color=red>红色非安全区</color>里会受到持续伤害</size></b>";
-		message_3 = "<b><size=15>     <color=red>操作说明</color> </size></b>"
+			message_3 = "<b><size=15>     <color=red>操作说明</color> </size></b>"
 			+ "\n\n<b><size=15>        <color=blue>↑ </color>键前进 </size></b>"
 			+ "\n<b><size=15>       <color=blue> ←</color>，<color=blue>→ </color>键控制方向</size></b>"
-			+ "\n<b><size=15>        <color=blue>Alt </color>键攻击</size></b>"
+			+ "\n<b><size=15>        <color=blue>J </color>键攻击</size></b>"
 			+ "\n<b><size=15>        <color=blue>U </color>，<color=blue>I </color>，<color=blue>O </color>，<color=blue>P </color>释放技能</size></b>"
 			+ "\n<b><size=15>        技能和攻击有 CD</size></b>";
+		}
 	}
 }
